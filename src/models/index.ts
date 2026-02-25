2dc18acc73e6722ba03d9a444680a352b4a8501b
rustyn <work.rustyn@gmail.com>
2026-02-25 21:55:20 +0530

    configure basic express app and typescript setup

---
+++ src/models/index.ts
@@ -0,0 +1,86 @@
+import { Sequelize, DataTypes, Model, Optional } from 'sequelize';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const sequelize = new Sequelize(process.env.DATABASE_URL as string, {
+    dialect: 'postgres',
+    dialectOptions: {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false,
+        },
+    },
+    logging: false,
+});
+
+interface ContactAttributes {
+    id: number;
+    phoneNumber: string | null;
+    email: string | null;
+    linkedId: number | null;
+    linkPrecedence: 'primary' | 'secondary';
+    createdAt: Date;
+    updatedAt: Date;
+    deletedAt: Date | null;
+}
+
+interface ContactCreationAttributes extends Optional<ContactAttributes, 'id' | 'linkedId' | 'deletedAt'> { }
+
+export class Contact extends Model<ContactAttributes, ContactCreationAttributes> implements ContactAttributes {
+    public id!: number;
+    public phoneNumber!: string | null;
+    public email!: string | null;
+    public linkedId!: number | null;
+    public linkPrecedence!: 'primary' | 'secondary';
+    public readonly createdAt!: Date;
+    public readonly updatedAt!: Date;
+    public deletedAt!: Date | null;
+}
+
+Contact.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            autoIncrement: true,
+            primaryKey: true,
+        },
+        phoneNumber: {
+            type: DataTypes.STRING,
+            allowNull: true,
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: true,
+        },
+        linkedId: {
+            type: DataTypes.INTEGER,
+            allowNull: true,
+        },
+        linkPrecedence: {
+            type: DataTypes.ENUM('primary', 'secondary'),
+            allowNull: false,
+            defaultValue: 'primary',
+        },
+        createdAt: {
+            type: DataTypes.DATE,
+            allowNull: false,
+        },
+        updatedAt: {
+            type: DataTypes.DATE,
+            allowNull: false,
+        },
+        deletedAt: {
+            type: DataTypes.DATE,
+            allowNull: true,
+        },
+    },
+    {
+        sequelize,
+        tableName: 'contacts',
+        paranoid: true,
+        timestamps: true,
+    }
+);
+
+export default sequelize;
